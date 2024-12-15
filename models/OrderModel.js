f4fbe9df2364a2e6ea3d7f848c88b9bcbd998676
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-12-15 20:25:28 +0300

    order model updated

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -3,9 +3,13 @@ import mongoose from 'mongoose';
 const OrderSchema = mongoose.Schema({
     cart_item: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'parentProduct' },
+        series: String,
         item_count: Number,
         quantity: Number,
         cogs: Number,
+        price: Number,
+        vendor_margin: Number,
+        order_status: String
     }],
     payment_at: Date,
 });
